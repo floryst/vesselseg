537aeee35df2af0ff8522c2ff72f8a5ebf396609
Forrest Li <forrest.li@kitware.com>
2017-09-11 13:40:44 -0400

    ENH: Represent application as own object

--- vesselseg/main.py
+++ vesselseg/main.py
@@ -4,15 +4,26 @@ import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 from PyQt4.QtGui import QApplication
+from PyQt4.QtCore import QObject
 from mainwindow import MainWindow
 
-def main():
-    app = QApplication(sys.argv)
+class VesselSegApp(QObject):
 
-    m = MainWindow()
-    m.show()
+    def __init__(self, parent=None):
+        super(VesselSegApp, self).__init__(parent)
 
-    return app.exec_()
+        self.qapp = QApplication(sys.argv)
+        self.window = MainWindow()
+
+    def run(self):
+        '''Runs the application.
+
+        Returns:
+            An integer representing the termination state.
+        '''
+        self.window.show()
+        return self.qapp.exec_()
 
 if __name__ == '__main__':
-    sys.exit(main())
+    app = VesselSegApp()
+    sys.exit(app.run())
